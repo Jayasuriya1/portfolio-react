be71d4554e511733b419f0a493a0fbcfbfcfc620
jayasuriya1 <jaya1999suriya1@gmail.com>
2023-09-13 20:33:42 +0530

    update

--- src/project.js
+++ src/project.js
@@ -17,7 +17,7 @@ export default function Project(){
                             </div>
                             <p>This is Fullstack MERN website made it as a CRM.
                                 <br></br>
-                                This application is used for reviewing customer contacts, leads and Service Requests.
+                                This application facilitates reviewing service requests and customer interactions.
                             </p>
                             <p style={{fontSize:"0.7rem"}}>
                                 <span style={{color:"#41c9ff"}}>TECT USED :</span>
@@ -28,6 +28,73 @@ export default function Project(){
                     </div>
                 </div>
                 <div className="col-sm-12 col-lg-6">
+                    <div class="card" style={{width: "18rem;"}}>
+                        <img src="/image/e-commerce.jpg" class="card-img-top" alt="..."></img>
+                        <div className="project-details">
+                            <h1 className="name" style={{color:"#ff4141"}}>E-Commerce</h1>
+                            <div>
+                                <a className="project-btn" href="https://github.com/Jayasuriya1/e-commerce---FE"  target="_blank" >Github FE <i class='bx bxl-github' ></i></a>
+                                <a className="project-btn" href="https://ecommerce-rsj.netlify.app/"  target="_blank">View Site <i class='bx bx-desktop'></i></a>
+                                <a className="project-btn" href="https://github.com/Jayasuriya1/E-Commerce---BE"  target="_blank">Github BE <i class='bx bxl-github' ></i></a>
+                            </div>
+                            <p>This is Fullstack MERN website made it as a Ecommerce.
+                                <br></br>
+                                This application is used to buy our products.
+                            </p>
+                            <br></br>
+                            <p style={{fontSize:"0.7rem"}}>
+                                <span style={{color:"#41c9ff"}}>TECT USED :</span>
+                                <span style={{color:"#d18d0d"}}>REACT, NODE, EXPRESS JS, MONGODB</span>
+                            </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-lg-6">
+                    <div class="card" style={{width: "18rem;"}}>
+                        <img src="/image/expense-tracker.jpg" class="card-img-top" alt="..."></img>
+                        <div className="project-details">
+                            <h1 className="name" style={{color:"#50C878"}}>Expense Tracker</h1>
+                            <div>
+                                <a className="project-btn" href="https://github.com/Jayasuriya1/expense-tracker-FE"  target="_blank" >Github FE <i class='bx bxl-github' ></i></a>
+                                <a className="project-btn" href="https://expense-tracker-rsj.netlify.app/"  target="_blank">View Site <i class='bx bx-desktop'></i></a>
+                                <a className="project-btn" href="https://github.com/Jayasuriya1/Expence-Tracker-BE"  target="_blank">Github BE <i class='bx bxl-github' ></i></a>
+                            </div>
+                            <p>This is Fullstack MERN website made it as a Expense Tracker.
+                                <br></br>
+                                This application is used to track the expense and income.
+                            </p>
+                            <p style={{fontSize:"0.7rem"}}>
+                                <span style={{color:"#41c9ff"}}>TECT USED :</span>
+                                <span style={{color:"#d18d0d"}}>REACT, NODE, EXPRESS JS, MONGODB</span>
+                            </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-lg-6">
+                    <div class="card" style={{width: "18rem;"}}>
+                        <img src="/image/note-taker.jpg" class="card-img-top" alt="..."></img>
+                        <div className="project-details">
+                            <h1 className="name" style={{color:"#FCE205"}}>Notify</h1>
+                            <div>
+                                <a className="project-btn" href="https://github.com/Jayasuriya1/to-do-list-FE"  target="_blank" >Github FE <i class='bx bxl-github' ></i></a>
+                                <a className="project-btn" href="https://notify1.netlify.app/"  target="_blank">View Site <i class='bx bx-desktop'></i></a>
+                                <a className="project-btn" href="https://github.com/Jayasuriya1/To-Do-List-BE"  target="_blank">Github BE <i class='bx bxl-github' ></i></a>
+                            </div>
+                            <p>This is Fullstack MERN website made it as a Notify.
+                                <br></br>
+                                This application is used to take a note.
+                            </p>
+                            <p style={{fontSize:"0.7rem"}}>
+                                <span style={{color:"#41c9ff"}}>TECT USED :</span>
+                                <span style={{color:"#d18d0d"}}>REACT, NODE, EXPRESS JS, MONGODB</span>
+                            </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="col-sm-12 col-lg-6">
                     <div class="card" style={{width: "18rem;"}}>
                         <img src="/image/url-shortener.jpg" class="card-img-top" alt="..."></img>
                         <div className="project-details">
@@ -88,8 +155,8 @@ export default function Project(){
                             </p>
                             
                         </div>
-                    </div>
-                </div>
+                    </div> 
+                </div>*/}
             </div>
         </div>
     )
